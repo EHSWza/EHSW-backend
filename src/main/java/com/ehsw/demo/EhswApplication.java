0f73c5d9ab9ed60162aa1566287621cb4487c843
SNComputerGuru <shillingndivho4@gmail.com>
2025-11-14 02:59:10 +0200

    initial commit

---
+++ src/main/java/com/ehsw/demo/EhswApplication.java
@@ -0,0 +1,13 @@
+package com.ehsw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhswApplication.class, args);
+	}
+
+}
